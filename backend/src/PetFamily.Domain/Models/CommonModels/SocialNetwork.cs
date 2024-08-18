252dbdfc0e2b4c0332c5c2e36c5ca2a3a17a5873
roman-net8 <roman-net@outlook.com>
2024-08-18 17:09:51 +0300

    ID-B-2/created pet model

---
+++ backend/src/PetFamily.Domain/Models/CommonModels/SocialNetwork.cs
@@ -0,0 +1,9 @@
+﻿namespace PetFamily.Domain.Models.CommonModels
+{
+    public class SocialNetwork
+    {
+        public string Title { get; private set; } = default!;
+
+        public string Url { get; private set; } = default!;
+    }
+}
