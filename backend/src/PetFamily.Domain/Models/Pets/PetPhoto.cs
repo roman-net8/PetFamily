9b4bffe135c776edc7a09b9b9ac8ccd7c7e31d6a
roman-net8 <roman-net@outlook.com>
2024-08-18 21:28:54 +0300

    created photo model

---
+++ backend/src/PetFamily.Domain/Models/Pets/PetPhoto.cs
@@ -0,0 +1,11 @@
+﻿namespace PetFamily.Domain.Models.Pets
+{
+    public class PetPhoto
+    {
+        public Guid Id { get; private set; }
+
+        public string StoragePath { get; private set; } = default!;
+
+        public bool IsMain { get; private set; }
+    }
+}
