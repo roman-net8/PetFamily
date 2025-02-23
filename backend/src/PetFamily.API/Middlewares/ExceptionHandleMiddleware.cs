6aa2ee10137f5979408e26bd7da087b031e39832
roman-net8 <roman-net@outlook.com>
2025-02-24 01:30:00 +0300

    B-5-5 Add exception handle middleware

---
+++ backend/src/PetFamily.API/Middlewares/ExceptionHandleMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using PetFamily.API.Response;
+
+namespace PetFamily.API.Middlewares;
+
+public class ExceptionHandleMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionHandleMiddleware> _logger;
+
+    public ExceptionHandleMiddleware(RequestDelegate next,
+        ILogger<ExceptionHandleMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+
+            var responseError = new ResponseError("server.internal", ex.Message, null);
+            var envelope = Envelope.Error([responseError]);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(envelope);
+        }
+    }
+}
+
+public static class ExceptionHandleMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandleMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ExceptionHandleMiddleware>();
+    }
+}
